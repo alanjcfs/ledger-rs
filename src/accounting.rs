9522df6c4a1d56704e8cda19a182f038dfecc1c8
Alan Fung-Schwarz <alan@atomsign.net>
2017-08-09 20:33:37 -0400

    Add partially working parser
    Use regex to split line
    
    Keep comments simple for now
    
    Implement Transaction and Account
    
    A Transaction consists of a payee, the date, and the accounts that were
    changed. An Account is the account with a name and balance.
    
    The simplest transaction has only two accounts, one to debit and one to
    credit. Anything more complicated than that tends to be a split
    transaction.
    
    Unwrap, damn you, unwrap
    
    Switch to ISO date format
    
    We'll worry about parsing other date formats in the future.
    
    Cleanup and use ledger to accept list of transactions
    
    Use enum in Balance
    
    Create Balance enum
    
    Add check_extra_empty_accounts
    
    Rename example.ledger to example.journal
    
    Split lib.rs into multiple files
    
    Rustfmt
    
    Remove comment
    
    Rustfmt lib.rs

---
+++ src/accounting.rs
@@ -0,0 +1,70 @@
+use chrono::prelude::{Date, Utc};
+
+#[derive(Debug, Clone)]
+pub enum Balance {
+    Amount(f64),
+    Empty,
+}
+
+impl Balance {
+    pub fn is_empty(self: Balance) -> bool {
+        match self {
+            Balance::Amount(_) => false,
+            Balance::Empty => true,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Account {
+    name: String,
+    balance: Balance,
+}
+
+impl Account {
+    pub fn new(s: String, f: Balance) -> Account {
+        Account {
+            name: s,
+            balance: f,
+        }
+    }
+    pub fn balance(self: &Account) -> &Balance {
+        &self.balance
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Transaction {
+    payee: String,
+    date: Date<Utc>,
+    account_changes: Vec<Account>,
+}
+
+impl Transaction {
+    pub fn new_default() -> Transaction {
+        Transaction {
+            payee: "".to_string(),
+            date: Utc::today(),
+            account_changes: vec![],
+        }
+    }
+    pub fn new(p: String, d: Date<Utc>, a: Vec<Account>) -> Transaction {
+        Transaction {
+            payee: p,
+            date: d,
+            account_changes: a,
+        }
+    }
+    pub fn add_account(mut self: Transaction, a: Account) -> Transaction {
+        self.account_changes.push(a);
+        self
+    }
+    pub fn change_payee_and_date(mut self: Transaction, s: &String, d: &Date<Utc>) -> Transaction {
+        self.date = d.clone();
+        self.payee = s.clone();
+        self
+    }
+    pub fn account_changes(self: &Transaction) -> &Vec<Account> {
+        &self.account_changes
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+extern crate clap;
+extern crate ledger;
+
+use clap::{App, Arg, SubCommand};
+use ledger::accounting::Transaction;
+
+fn main() {
+    let matches = App::new("ledger-rs")
+        .version("0.1")
+        .about("Rust port of Ledger CLI")
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .help("Set the file to use")
+                .takes_value(true),
+        )
+        .subcommand(SubCommand::with_name("balance"))
+        .subcommand(SubCommand::with_name("budget"))
+        .get_matches();
+
+    let file = matches
+        .value_of("file")
+        .unwrap_or("examples/example.journal");
+
+    let contents = ledger::read::read(file).unwrap();
+    let lines = contents.lines();
+    let mut ledger: Vec<Option<Transaction>> = Vec::new();
+    ledger::parse::parse(lines, &mut ledger);
+
+    // When balance passed, print out transactions
+    //    20  Assets
+    //  -600    Bank of America
+    //    20    Cash
+    //   600    Savings
+    // -1720  Equity:Opening Balance
+    //  1720  Expenses
+    //   160    Food
+    //  1560    Rent
+    //  ----
+    //     0
+    //
+    // In each transaction, the accounts should add up to zero to be balanced.
+    // We need to store the total ins and outs of each account too.
+
+    ledger::check_extra_empty_accounts(&ledger);
+    ledger::verify(ledger.clone());
+    println!("{:?}", ledger);
+}
