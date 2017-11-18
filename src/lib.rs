e3d905c6f35e07f3d7a2b7cac2d5804c976108a3
Alan Fung-Schwarz <alan@atomsign.net>
2017-11-17 19:00:00 -0500

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,22 @@ pub mod lexer;
 pub mod status;
 pub mod error;
 
-
+/*
+ * # TRANSACTIONS
+ *
+ * Contains only date, edate, clear status, code, description
+ *
+ * # POSTINGS
+ *
+ * Linked to transaction and account (a single record per account seems the simplest
+ * implementation, more like how a database would handle it, than trying to store multiple accounts
+ * in an array. It's also probably the easiest way to export and import into SQLite)
+ *
+ * # ACCOUNTS
+ *
+ * Contains amounts?
+ * Has a name, which can be colon separated.
+ */
 
 
 #[cfg(test)]
