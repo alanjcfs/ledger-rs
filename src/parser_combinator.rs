949f7256ce8fb0658ce12de8c52781d73f75c598
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2017-12-29 13:03:02 -0800

    Move implementation into trait with Addition struct

--- src/parser_combinator.rs
+++ src/parser_combinator.rs
@@ -52,39 +52,6 @@ enum Match {
 #[derive(Debug, PartialEq)]
 struct MatchState(Match, State);
 
-fn str_(s: String) -> Box<Fn(State) -> Option<MatchState>> {
-    Box::new(move |state| {
-        let chunk = state.peek(s.len());
-
-        if let Some(chunk) = chunk {
-            if chunk == s {
-                Some(MatchState(Match::Str(chunk), state.read(s.len())))
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    })
-}
-
-fn chr(pattern: String) -> Box<Fn(State) -> Option<MatchState>> {
-    Box::new(move |state| {
-        let chunk = state.peek(1);
-
-        let pattern_regex = Regex::new(&format!("[{}]", pattern)).unwrap();
-        if let Some(chunk) = chunk {
-            if pattern_regex.is_match(&chunk) {
-                Some(MatchState(Match::Chr(chunk), state.read(1)))
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    })
-}
-
 #[derive(Debug, Clone)]
 enum Func {
     Str(String),
@@ -94,117 +61,163 @@ enum Func {
     Alt(Vec<Func>),
 }
 
-fn seq(combinators: Vec<Func>) -> Box<Fn(State) -> Option<MatchState>> {
-    Box::new(move |state| {
-        let mut matches = Vec::new();
-        // Feed input state through a chain of other combinators, output state of one combinator
-        // becomes the input for the next. If all combinators match, return the sequence
-        // with state set to the last state otherwise, return None.
-        let mut current_state = Some(state.clone());
-
-        for combinator in &combinators {
-            let result = match combinator {
-                &Func::Str(ref s) => {
-                    str_(s.to_owned())(current_state.clone().unwrap())
-                }
-                &Func::Chr(ref s) => {
-                    chr(s.to_owned())(current_state.clone().unwrap())
-                }
-                &Func::Rep(ref s, size) => {
-                    rep(*s.to_owned(), size)(current_state.clone().unwrap())
-                }
-                &Func::Seq(ref s) => {
-                    seq(s.clone().to_vec())(current_state.clone().unwrap())
-                }
-                &Func::Alt(ref s) => {
-                    alt(s.clone().to_vec())(current_state.clone().unwrap())
-                }
-            };
-            if let Some(MatchState(node, new_state)) = result {
-                matches.push(node);
-                current_state = Some(new_state.clone());
-            }
-        }
-
-        if current_state.is_some(){
-            Some(MatchState(Match::Seq(matches), current_state.unwrap()))
-        }
-        else {
-            None
-        }
-    })
+trait ParserCombinator {
+    fn str_(s: String) -> Box<Fn(State) -> Option<MatchState>>;
+    fn chr(pattern: String) -> Box<Fn(State) -> Option<MatchState>>;
+    fn seq(combinators: Vec<Func>) -> Box<Fn(State) -> Option<MatchState>>;
+    fn rep(combinator: Func, n: usize) -> Box<Fn(State) -> Option<MatchState>>;
+    fn alt(parsers: Vec<Func>) -> Box<Fn(State) -> Option<MatchState>>;
 }
 
-fn rep(combinator: Func, n: usize) -> Box<Fn(State) -> Option<MatchState>> {
-    Box::new(move |state| {
-        let mut matches = Vec::new();
-        let mut last_state = None;
-        let mut current_state = Some(state.clone());
+struct Addition;
 
-        while current_state.is_some() {
-            let s = current_state.clone().unwrap();
-            last_state = Some(s.clone());
-            let result = match combinator {
-                Func::Str(ref f) => {
-                    str_(f.to_owned())(s.clone())
+impl ParserCombinator for Addition {
+    fn str_(s: String) -> Box<Fn(State) -> Option<MatchState>> {
+        Box::new(move |state| {
+            let chunk = state.peek(s.len());
+
+            if let Some(chunk) = chunk {
+                if chunk == s {
+                    Some(MatchState(Match::Str(chunk), state.read(s.len())))
+                } else {
+                    None
                 }
-                Func::Chr(ref f) => {
-                    chr(f.to_owned())(s.clone())
-                }
-                Func::Rep(ref f, size) => {
-                    rep(*f.to_owned(), size)(s.clone())
-                }
-                Func::Seq(ref f) => {
-                    seq(f.clone().to_vec())(s.clone())
-                }
-                Func::Alt(ref f) => {
-                    alt(f.clone().to_vec())(s.clone())
-                }
-            };
-            if let Some(MatchState(node, new_state)) = result {
-                current_state = Some(new_state);
-                matches.push(node);
             } else {
-                current_state = None;
+                None
             }
-        }
+        })
+    }
 
-        if matches.len() >= n {
-            Some(MatchState(Match::Rep(matches), last_state.unwrap().clone()))
-        } else {
+    fn chr(pattern: String) -> Box<Fn(State) -> Option<MatchState>> {
+        Box::new(move |state| {
+            let chunk = state.peek(1);
+
+            let pattern_regex = Regex::new(&format!("[{}]", pattern)).unwrap();
+            if let Some(chunk) = chunk {
+                if pattern_regex.is_match(&chunk) {
+                    Some(MatchState(Match::Chr(chunk), state.read(1)))
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+    }
+
+
+    fn seq(combinators: Vec<Func>) -> Box<Fn(State) -> Option<MatchState>> {
+        Box::new(move |state| {
+            let mut matches = Vec::new();
+            // Feed input state through a chain of other combinators, output state of one combinator
+            // becomes the input for the next. If all combinators match, return the sequence
+            // with state set to the last state otherwise, return None.
+            let mut current_state = Some(state.clone());
+
+            for combinator in &combinators {
+                let result = match combinator {
+                    &Func::Str(ref s) => {
+                        Self::str_(s.to_owned())(current_state.clone().unwrap())
+                    }
+                    &Func::Chr(ref s) => {
+                        Self::chr(s.to_owned())(current_state.clone().unwrap())
+                    }
+                    &Func::Rep(ref s, size) => {
+                        Self::rep(*s.to_owned(), size)(current_state.clone().unwrap())
+                    }
+                    &Func::Seq(ref s) => {
+                        Self::seq(s.clone().to_vec())(current_state.clone().unwrap())
+                    }
+                    &Func::Alt(ref s) => {
+                        Self::alt(s.clone().to_vec())(current_state.clone().unwrap())
+                    }
+                };
+                if let Some(MatchState(node, new_state)) = result {
+                    matches.push(node);
+                    current_state = Some(new_state.clone());
+                }
+            }
+
+            if current_state.is_some(){
+                Some(MatchState(Match::Seq(matches), current_state.unwrap()))
+            }
+            else {
+                None
+            }
+        })
+    }
+
+    fn rep(combinator: Func, n: usize) -> Box<Fn(State) -> Option<MatchState>> {
+        Box::new(move |state| {
+            let mut matches = Vec::new();
+            let mut last_state = None;
+            let mut current_state = Some(state.clone());
+
+            while current_state.is_some() {
+                let s = current_state.clone().unwrap();
+                last_state = Some(s.clone());
+                let result = match combinator {
+                    Func::Str(ref f) => {
+                        Self::str_(f.to_owned())(s.clone())
+                    }
+                    Func::Chr(ref f) => {
+                        Self::chr(f.to_owned())(s.clone())
+                    }
+                    Func::Rep(ref f, size) => {
+                        Self::rep(*f.to_owned(), size)(s.clone())
+                    }
+                    Func::Seq(ref f) => {
+                        Self::seq(f.clone().to_vec())(s.clone())
+                    }
+                    Func::Alt(ref f) => {
+                        Self::alt(f.clone().to_vec())(s.clone())
+                    }
+                };
+                if let Some(MatchState(node, new_state)) = result {
+                    current_state = Some(new_state);
+                    matches.push(node);
+                } else {
+                    current_state = None;
+                }
+            }
+
+            if matches.len() >= n {
+                Some(MatchState(Match::Rep(matches), last_state.unwrap().clone()))
+            } else {
+                None
+            }
+
+        })
+    }
+
+    fn alt(parsers: Vec<Func>) -> Box<Fn(State) -> Option<MatchState>> {
+        Box::new(move |state| {
+            for parser in &parsers {
+                let r = match parser {
+                    &Func::Str(ref f) => {
+                        Self::str_(f.to_owned())(state.clone())
+                    }
+                    &Func::Chr(ref f) => {
+                        Self::chr(f.to_owned())(state.clone())
+                    }
+                    &Func::Rep(ref f, size) => {
+                        Self::rep(*f.to_owned(), size)(state.clone())
+                    }
+                    &Func::Seq(ref f) => {
+                        Self::seq(f.clone().to_vec())(state.clone())
+                    }
+                    &Func::Alt(ref f) => {
+                        Self::alt(f.clone().to_vec())(state.clone())
+                    }
+                };
+                if let Some(result) = r {
+                    return Some(result)
+                }
+            }
+
             None
-        }
-
-    })
-}
-
-fn alt(parsers: Vec<Func>) -> Box<Fn(State) -> Option<MatchState>> {
-    Box::new(move |state| {
-        for parser in &parsers {
-            let r = match parser {
-                &Func::Str(ref f) => {
-                    str_(f.to_owned())(state.clone())
-                }
-                &Func::Chr(ref f) => {
-                    chr(f.to_owned())(state.clone())
-                }
-                &Func::Rep(ref f, size) => {
-                    rep(*f.to_owned(), size)(state.clone())
-                }
-                &Func::Seq(ref f) => {
-                    seq(f.clone().to_vec())(state.clone())
-                }
-                &Func::Alt(ref f) => {
-                    alt(f.clone().to_vec())(state.clone())
-                }
-            };
-            if let Some(result) = r {
-                return Some(result)
-            }
-        }
-
-        None
-    })
+        })
+    }
 }
 
 #[cfg(test)]
@@ -240,8 +253,8 @@ mod tests {
     #[test]
     fn test_str() {
         let input = State::new("hello world", 0);
-        let hello = str_(r"hello".to_string());
-        let world = str_(r"world".to_string());
+        let hello = Addition::str_(r"hello".to_string());
+        let world = Addition::str_(r"world".to_string());
 
         if let Some(MatchState(m, s)) = hello(input) {
             assert_eq!(m, Match::Str("hello".to_string()));
@@ -263,7 +276,7 @@ mod tests {
     #[test]
     fn test_chr() {
         let input = State::new("12 + 34", 0);
-        let digit = chr(r"0-9".to_string());
+        let digit = Addition::chr(r"0-9".to_string());
 
         if let Some(MatchState(m, s)) = digit(input.read(1)) {
             assert_eq!(m, Match::Chr("2".to_string()));
@@ -286,14 +299,14 @@ mod tests {
     fn test_seq() {
         let input = State::new("7+8", 0);
         // Sanity check
-        let digit = chr(r"0-9".to_string());
-        let reg = str_("+".to_string());
+        let digit = Addition::chr(r"0-9".to_string());
+        let reg = Addition::str_("+".to_string());
         assert_eq!(digit(input.read(0)), Some(MatchState(Match::Chr("7".to_string()), State::new("7+8", 1))));
         assert_eq!(reg(input.read(1)), Some(MatchState(Match::Str("+".to_string()), State::new("7+8", 2))));
         assert_eq!(digit(input.read(2)), Some(MatchState(Match::Chr("8".to_string()), State::new("7+8", 3))));
 
 
-        let addition = seq(vec![Func::Chr("0-9".to_string()), Func::Str(r"+".to_string()), Func::Chr("0-9".to_string())]);
+        let addition = Addition::seq(vec![Func::Chr("0-9".to_string()), Func::Str(r"+".to_string()), Func::Chr("0-9".to_string())]);
 
         let results = addition(input);
         if let Some(MatchState(m, s)) = results {
@@ -314,7 +327,7 @@ mod tests {
     #[test]
     fn test_rep() {
         let input = State::new("2017", 0);
-        let number = rep(Func::Chr("0-9".to_string()), 1);
+        let number = Addition::rep(Func::Chr("0-9".to_string()), 1);
         let results = number(input);
         if let Some(MatchState(m, s)) = results {
             assert_eq!(
@@ -350,7 +363,7 @@ mod tests {
             w.clone(),
             Func::Str("+".to_string()), w.clone(), number.clone()
             ]);
-        let expression = alt(vec![addition, number.clone()]);
+        let expression = Addition::alt(vec![addition, number.clone()]);
         let result = expression(State::new("12", 0));
         if let Some(MatchState(m, s)) = result {
             assert_eq!(s,
